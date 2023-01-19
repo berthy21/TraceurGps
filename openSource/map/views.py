d53bd3085ae3bb0238ca391c41f407605dc697f5
berthy21 <berthy133@gmail.com>
2023-01-19 16:50:29 +0100

    add map to projetc

--- openSource/map/views.py
+++ openSource/map/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+import folium 
+from folium import plugins
 
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    #creation de la carte
+    m = folium.Map(location = [50,-5])
+
+    plugins.LocateControl(auto_start=True).add_to(m)
+    
+    folium.Marker()
+    #la representation html de l'objet carte 
+    m =  m._repr_html_()
+    context = {
+        'm':m,
+    }
+    return render(request,'index.html',context)
